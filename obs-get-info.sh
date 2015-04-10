c19e443862db3ed41241038eb117585b9a386bc0
Dmitriy A. Perlow <dap.darkness@gmail.com>
2015-04-11 00:58:56 +0300

    Added: openSUSE Build Service runtime information:  * main.c compressing;  * `ls -l (main.c compressed)`

--- obs-get-info.sh
+++ obs-get-info.sh
@@ -1,1 +1,1 @@
-curl -s https://build.opensuse.org/build/home:DarkSS/openSUSE_Factory/x86_64/HuffmanArchiver/_log | grep -vE "mkswap|clang" | grep -E " warning: |scan-build:| error: "
+curl -s https://build.opensuse.org/build/home:DarkSS/openSUSE_Factory/x86_64/HuffmanArchiver/_log | grep -vE "mkswap|clang" | grep -E " warning: |scan-build:| error: |\.ha"
